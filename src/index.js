7a94467de36012086b9423cf1cf6b766ea296c2f
RomarioNik <115925888+RomarioNik@users.noreply.github.com>
2023-08-26 20:13:30 +0100

    fix normalize

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from 'components/App';
+import './normalize.css';
 import './index.css';
-
 import { Provider } from 'react-redux';
 import { persistor, store } from 'redux/store';
 import { PersistGate } from 'redux-persist/integration/react';

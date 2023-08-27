f7f7f844e25870b6f8a3a6b46b90d469b4894a60
Str0ng3r <116167012+Str0ng3r@users.noreply.github.com>
2023-08-27 20:09:43 +0200

    NewBoard

---
+++ src/components/NewEditBoard/NewBoard.jsx
@@ -0,0 +1,13 @@
+
+import { Icon } from "img/svg/Icon"
+import styles from './NewBoard.module.css';
+
+const NewBoard = () => {
+
+
+return (
+    <div className={styles.divCard}>
+
+    </div>
+)
+ }

23437e3eae44fd520b504158e83290f26b7003ae
Zerossik <zeross19940303@gmail.com>
2023-08-30 01:24:39 +0300

    WelcomPage done

---
+++ src/img/png/index.js
@@ -0,0 +1,4 @@
+export { default as startUser_124 } from './mobile/start-user-124.png';
+export { default as startUser_x2_248 } from './mobile/start-user@2x-248.png';
+export { default as startUser_desctop_162 } from './desctop/start-user_desctop-162.png';
+export { default as startUser_x2_desctop_324 } from './desctop/start-user_desctop@2x-324.png';

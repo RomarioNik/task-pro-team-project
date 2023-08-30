3d510c7fc5880e5163b99918f68bf6e5c4ba7527
Illia Datsenko <datsenko.i@gmail.com>
2023-08-30 18:13:07 +0300

    added operations for cards and columns

--- src/redux/boards/boardsSlice.js
+++ src/redux/boards/boardsSlice.js
@@ -5,6 +5,12 @@ import {
   addBoard,
   deleteBoard,
   updateBoard,
+  addColumn,
+  deleteColumn,
+  updateColumnById,
+  addCard,
+  deleteCard,
+  updateCardById,
 } from './boardsOperations';
 
 const handlePending = state => {
@@ -43,8 +49,60 @@ const handleFulfilledDeleteBoard = (state, { payload }) => {
 const handleFulfilledUpdateBoard = (state, { payload }) => {
   state.isLoading = false;
   state.error = null;
-  state.allBoards = state.items.map(el =>
+  state.allBoards = state.allBoards.map(el =>
     el._id === payload._id ? (el = payload) : el
+  );
+};
+
+const handleFulfilledAddColumn = (state, { payload }) => {
+  state.isLoading = false;
+  state.error = null;
+  state.shownBoard.columns.push(payload);
+};
+
+const handleFulfilledDeleteColumn = (state, { payload }) => {
+  state.isLoading = false;
+  state.error = null;
+  state.shownBoard.columns = state.shownBoard.columns.filter(
+    ({ _id }) => _id !== payload
+  );
+};
+
+const handleFulfilledUpdateColumnById = (state, { payload }) => {
+  state.isLoading = false;
+  state.error = null;
+  state.shownBoard.columns = state.shownBoard.columns.map(el =>
+    el._id === payload._id ? (el = payload) : el
+  );
+};
+
+const handleFulfilledAddCard = (state, { payload }) => {
+  state.isLoading = false;
+  state.error = null;
+  state.shownBoard.columns.find(el =>
+    el._id === payload._id ? el.cards.push(payload) : el
+  );
+};
+
+const handleFulfilledDeleteCard = (state, { payload }) => {
+  state.isLoading = false;
+  state.error = null;
+  console.log(payload);
+  //знайти по айді колонку, в ній по айді відфільтрувати зайву картку
+  state.shownBoard.columns.find(el =>
+    el._id === payload.column
+      ? el.cards.filter(({ _id }) => _id !== payload._id)
+      : el
+  );
+};
+
+const handleFulfilledUpdateCardById = (state, { payload }) => {
+  state.isLoading = false;
+  state.error = null;
+  state.shownBoard.columns.find(el =>
+    el._id === payload.column
+      ? el.cards.map(el => (el._id === payload._id ? (el = payload) : el))
+      : el
   );
 };
 
@@ -65,6 +123,13 @@ const boardsSlice = createSlice({
       .addCase(addBoard.fulfilled, handleFulfilledAddBoard)
       .addCase(deleteBoard.fulfilled, handleFulfilledDeleteBoard)
       .addCase(updateBoard.fulfilled, handleFulfilledUpdateBoard)
+      .addCase(addColumn.fulfilled, handleFulfilledAddColumn)
+      .addCase(deleteColumn.fulfilled, handleFulfilledDeleteColumn)
+      .addCase(updateColumnById.fulfilled, handleFulfilledUpdateColumnById)
+      .addCase(addCard.fulfilled, handleFulfilledAddCard)
+      .addCase(deleteCard.fulfilled, handleFulfilledDeleteCard)
+      .addCase(updateCardById.fulfilled, handleFulfilledUpdateCardById)
+
       .addMatcher(action => action.type.endsWith('/pending'), handlePending)
       .addMatcher(action => action.type.endsWith('/rejected'), handleRejected),
 });

e86124afb6910f7415e585f5c54e5360a92bedec
Zerossik <zeross19940303@gmail.com>
2023-08-30 18:11:06 +0300

    !

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,6 +1,7 @@
 import LoginForm from 'components/LoginForm/LoginForm';
 import RegisterForm from 'components/RegisterForm/RegisterForm';
 import React from 'react';
+import css from './AuthPage.module.css';
 // import { Outlet } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 
@@ -8,7 +9,7 @@ const AuthPage = () => {
   const { id } = useParams();
 
   return (
-    <div>
+    <div className={css.authPage}>
       <p>AuthPage</p>
       {id === 'login' ? <LoginForm /> : <RegisterForm />}
       {/* <Outlet /> */}

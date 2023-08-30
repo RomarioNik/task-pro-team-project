530d46ae46ebc2c7976bd59243d3a2fdb936cb91
Sofiia Shmaiun <sofiia.shmaiun@gmail.com>
2023-08-30 17:31:49 +0300

    add modal windows

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,61 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import css from './Sidebar.module.css';
 import sprite from '../../img/svg/sprite-icon.svg';
 import { useDispatch } from 'react-redux';
 import { logOutUser } from 'redux/auth/operations';
 
+import {
+  deleteBoard,
+  getAllBoards,
+  // addBoard,
+} from '../../redux/boards/boardsOperations';
+import { useBoardsList } from 'hooks/useBoardsList';
+import Modal from '../Modal/index.js';
+import NeedHelp from '../Needhelp/NeedHelp';
+import NewBoard from '../NewEditBoard/NewBoard';
+
 const Sidebar = ({ isOpen }) => {
-  //дошки будуть приходити з бекенду, назви рендеряться в сайдбарі
-  const boards = [
-    {
-      name: 'Project office',
-      id: '1',
-    },
-    {
-      name: 'Neon Light Project Test',
-      id: '2',
-    },
-    {
-      name: 'Board3',
-      id: '3',
-    },
-    {
-      name: 'Board4',
-      id: '4',
-    },
-    {
-      name: 'Board5',
-      id: '5',
-    },
-  ];
+  const [openNeedHelpModal, setOpenNeedHelpModal] = useState(false);
+  const [openNewBoardModal, setOpenNewBoardModal] = useState(false);
+  const [openEditBoardModal, setOpenEditBoardModal] = useState(false);
 
   const dispatch = useDispatch();
 
-  const handleNeedHelp = () => {
-    console.log('Modal window Need Help');
-  };
+  const boards = useBoardsList();
+
+  useEffect(() => {
+    dispatch(getAllBoards());
+  }, [dispatch]);
 
   const handleCreateBoard = () => {
-    console.log('Modal window New Board');
+    setOpenNewBoardModal(true);
+
+    // const newBoard = {
+    //   title: 'Test new board 2',
+    //   icon: 'project',
+    //   background: 'magnolia',
+    // };
+    // dispatch(addBoard(newBoard));
+  };
+
+  const handleNeedHelp = () => {
+    setOpenNeedHelpModal(true);
   };
 
   const handleEditBoard = () => {
-    console.log('Modal window Edit Board');
-  };
-
-  const handleDeleteBoard = (boardId, name) => {
-    const indexToDelete = boards.findIndex(obj => obj.id === boardId);
-    if (indexToDelete !== -1) {
-      boards.splice(indexToDelete, 1);
-    }
-
-    console.log(boards);
-    console.log(window.location.pathname);
+    setOpenEditBoardModal(true);
   };
 
   return (
     <div className={isOpen ? css.active : css.sidebar}>
+      {openNeedHelpModal && (
+        <Modal children={<NeedHelp />} openModal={setOpenNeedHelpModal} />
+      )}
+      {openNewBoardModal && (
+        <Modal children={<NewBoard />} openModal={setOpenNewBoardModal} />
+      )}
+      {openEditBoardModal && (
+        <Modal children={<NewBoard />} openModal={setOpenEditBoardModal} />
+      )}
       <Link to="/home" className={css.logo}>
         <svg width="32" height="32" className={css.logoIcon}>
           <use xlinkHref={`${sprite}#logo`} />
@@ -74,19 +76,19 @@ const Sidebar = ({ isOpen }) => {
           </button>
         </div>
         <ul className={css.boardsList}>
-          {boards.map(({ name, id }) => (
-            <li key={id}>
+          {boards.map(({ _id, title, icon }) => (
+            <li key={_id}>
               <NavLink
-                to={`${name}`}
+                to={`${title}`}
                 className={({ isActive }) =>
                   isActive ? css.active : css.boardsItem
                 }
               >
                 <div className={css.boardTitle}>
                   <svg width="18" height="18" className={css.boardIcon}>
-                    <use xlinkHref={`${sprite}#puzzle-piece`} />
+                    <use xlinkHref={`${sprite}#${icon}`} />
                   </svg>
-                  <p className={css.boardName}>{name}</p>
+                  <p className={css.boardName}>{title}</p>
                 </div>
                 <div className={css.boardTools}>
                   <button onClick={handleEditBoard}>
@@ -94,7 +96,8 @@ const Sidebar = ({ isOpen }) => {
                       <use xlinkHref={`${sprite}#pencil`} />
                     </svg>
                   </button>
-                  <button onClick={() => handleDeleteBoard(id, name)}>
+
+                  <button onClick={() => dispatch(deleteBoard(_id))}>
                     <svg width="16" height="16" className={css.editBoardIcon}>
                       <use xlinkHref={`${sprite}#trash`} />
                     </svg>
@@ -136,165 +139,3 @@ const Sidebar = ({ isOpen }) => {
 };
 
 export default Sidebar;
-
-// import React, { useEffect } from 'react';
-// import { Link, NavLink } from 'react-router-dom';
-// import css from './Sidebar.module.css';
-// import sprite from '../../img/svg/sprite-icon.svg';
-// import { useDispatch } from 'react-redux';
-// import { logOutUser } from 'redux/auth/operations';
-// import {
-//   addBoard,
-//   deleteBoard,
-//   getAllBoards,
-// } from 'redux/boards/boardsOperations';
-// import { useBoardsList } from 'hooks/useBoardsList';
-
-// const Sidebar = ({ isOpen }) => {
-//   //дошки будуть приходити з бекенду, назви рендеряться в сайдбарі
-//   // const boards = [
-//   //   {
-//   //     name: 'Project office',
-//   //     id: '1',
-//   //   },
-//   //   {
-//   //     name: 'Neon Light Project Test',
-//   //     id: '2',
-//   //   },
-//   //   {
-//   //     name: 'Board3',
-//   //     id: '3',
-//   //   },
-//   //   {
-//   //     name: 'Board4',
-//   //     id: '4',
-//   //   },
-//   //   {
-//   //     name: 'Board5',
-//   //     id: '5',
-//   //   },
-//   // ];
-
-//   const boards = useBoardsList();
-//   console.log(boards);
-
-//   const dispatch = useDispatch();
-
-//   const handleNeedHelp = () => {
-//     console.log('Modal window Need Help');
-//   };
-
-//   const handleCreateBoard = () => {
-//     console.log('Modal window New Board');
-
-//     const newBoard = {
-//       title: 'Test new board',
-//       icon: 'project',
-//       background: 'magnolia',
-//     };
-
-//     dispatch(addBoard(newBoard));
-//   };
-
-//   const handleEditBoard = () => {
-//     console.log('Modal window Edit Board');
-//   };
-
-//   const handleDeleteBoard = (boardId, name) => {
-//     // const indexToDelete = boards.findIndex(obj => obj.id === boardId);
-//     // if (indexToDelete !== -1) {
-//     //   boards.splice(indexToDelete, 1);
-//     // }
-
-//     // console.log(boards);
-//     // console.log(window.location.pathname);
-//     console.log(boardId);
-//     dispatch(deleteBoard(boardId));
-//   };
-
-//   useEffect(() => {
-//     dispatch(getAllBoards());
-//   }, [dispatch]);
-
-//   return (
-//     <div className={isOpen ? css.active : css.sidebar}>
-//       <Link to="/home" className={css.logo}>
-//         <svg width="32" height="32" className={css.logoIcon}>
-//           <use xlinkHref={`${sprite}#logo`} />
-//         </svg>
-//         <p className={css.logoText}>TaskPro</p>
-//       </Link>
-
-//       <div className={css.myBoards}>
-//         <p className={css.myBoardsText}>My Boards</p>
-//         <div className={css.createBoardSection}>
-//           <p className={css.createBoardText}>Create a new board</p>
-//           <button className={css.createBoardButton} onClick={handleCreateBoard}>
-//             <svg width="20" height="20" className={css.addIcon}>
-//               <use xlinkHref={`${sprite}#plus`} />
-//             </svg>
-//           </button>
-//         </div>
-//         <ul className={css.boardsList}>
-//           {boards.map(({ title, _id }) => (
-//             <li key={_id}>
-//               <NavLink
-//                 to={`${title}`}
-//                 className={({ isActive }) =>
-//                   isActive ? css.active : css.boardsItem
-//                 }
-//               >
-//                 <div className={css.boardTitle}>
-//                   <svg width="18" height="18" className={css.boardIcon}>
-//                     <use xlinkHref={`${sprite}#puzzle-piece`} />
-//                   </svg>
-//                   <p className={css.boardName}>{title}</p>
-//                 </div>
-//                 <div className={css.boardTools}>
-//                   <button onClick={handleEditBoard}>
-//                     <svg width="16" height="16" className={css.editBoardIcon}>
-//                       <use xlinkHref={`${sprite}#pencil`} />
-//                     </svg>
-//                   </button>
-//                   <button onClick={() => handleDeleteBoard(_id, title)}>
-//                     <svg width="16" height="16" className={css.editBoardIcon}>
-//                       <use xlinkHref={`${sprite}#trash`} />
-//                     </svg>
-//                   </button>
-//                 </div>
-//               </NavLink>
-//             </li>
-//           ))}
-//         </ul>
-//       </div>
-//       <div className={css.needHelpSection}>
-//         <div className={css.needHelpImage}></div>
-//         <p className={css.needHelpText}>
-//           If you need help with{' '}
-//           <Link to="/home" className={css.taskPro}>
-//             TaskPro
-//           </Link>
-//           , check out our support resources or reach out to our customer support
-//           team.
-//         </p>
-//         <button className={css.needHelpButton} onClick={handleNeedHelp}>
-//           <svg width="20" height="20" className={css.helpIcon}>
-//             <use xlinkHref={`${sprite}#help_circle`} />
-//           </svg>
-//           <p>Need help?</p>
-//         </button>
-//       </div>
-//       <button
-//         className={css.logOutButton}
-//         onClick={() => dispatch(logOutUser())}
-//       >
-//         <svg width="32" height="32" className={css.logoutIcon}>
-//           <use xlinkHref={`${sprite}#log-out`} />
-//         </svg>
-//         <p>Log out</p>
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default Sidebar;

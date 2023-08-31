046e4a9d135d2d2f45eeca17fbdf74a3d0610ff7
Valerii Pometun <vmpometun@gmail.com>
2023-08-31 13:14:25 +0300

    test

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,63 +1,63 @@
 import React, { useEffect } from 'react';
 import style from './Filter.module.css';
 import { useDispatch, useSelector } from 'react-redux';
-import { getFilter } from 'redux/boards/boardsSelectors';
+// import { getFilter } from 'redux/boards/boardsSelectors';
 import { addFilter } from 'redux/boards/filterSlice';
 
 const Filter = () => {
   const dispatch = useDispatch();
 
   // ===============Перевірка фільтрації
-  const currentFilter = useSelector(getFilter);
-  const cards = [
-    [
-      { id: 'first', priority: 'high', name: 'card-11' },
-      { id: 'first', priority: 'low', name: 'card-12' },
-      { id: 'first', priority: 'high', name: 'card-13' },
-      { id: 'first', priority: 'high', name: 'card-14' },
-      { id: 'first', priority: 'medium', name: 'card-15' },
-      { id: 'first', priority: 'without priority', name: 'card-16' },
-      { id: 'first', priority: 'high', name: 'card-17' },
-    ],
-    [
-      { id: 'second', priority: 'without priority', name: 'card-21' },
-      { id: 'second', priority: 'low', name: 'card-22' },
-      { id: 'second', priority: 'high', name: 'card-23' },
-      { id: 'second', priority: 'low', name: 'card-24' },
-      { id: 'second', priority: 'without-priority', name: 'card-25' },
-      { id: 'second', priority: 'without-priority', name: 'card-26' },
-      { id: 'second', priority: 'medium', name: 'card-27' },
-    ],
-    [
-      { id: 'third', priority: 'low', name: 'card-31' },
-      { id: 'third', priority: 'low', name: 'card-32' },
-      { id: 'third', priority: 'without priority', name: 'card-33' },
-      { id: 'third', priority: 'high', name: 'card-34' },
-      { id: 'third', priority: 'medium', name: 'card-35' },
-      { id: 'third', priority: 'without priority', name: 'card-36' },
-      { id: 'third', priority: 'high', name: 'card-37' },
-    ],
-  ];
+  // const currentFilter = useSelector(getFilter);
+  // const cards = [
+  //   [
+  //     { id: 'first', priority: 'high', name: 'card-11' },
+  //     { id: 'first', priority: 'low', name: 'card-12' },
+  //     { id: 'first', priority: 'high', name: 'card-13' },
+  //     { id: 'first', priority: 'high', name: 'card-14' },
+  //     { id: 'first', priority: 'medium', name: 'card-15' },
+  //     { id: 'first', priority: 'without priority', name: 'card-16' },
+  //     { id: 'first', priority: 'high', name: 'card-17' },
+  //   ],
+  //   [
+  //     { id: 'second', priority: 'without priority', name: 'card-21' },
+  //     { id: 'second', priority: 'low', name: 'card-22' },
+  //     { id: 'second', priority: 'high', name: 'card-23' },
+  //     { id: 'second', priority: 'low', name: 'card-24' },
+  //     { id: 'second', priority: 'without-priority', name: 'card-25' },
+  //     { id: 'second', priority: 'without-priority', name: 'card-26' },
+  //     { id: 'second', priority: 'medium', name: 'card-27' },
+  //   ],
+  //   [
+  //     { id: 'third', priority: 'low', name: 'card-31' },
+  //     { id: 'third', priority: 'low', name: 'card-32' },
+  //     { id: 'third', priority: 'without priority', name: 'card-33' },
+  //     { id: 'third', priority: 'high', name: 'card-34' },
+  //     { id: 'third', priority: 'medium', name: 'card-35' },
+  //     { id: 'third', priority: 'without priority', name: 'card-36' },
+  //     { id: 'third', priority: 'high', name: 'card-37' },
+  //   ],
+  // ];
 
-  const filtered = (f, cards) => {
-    if (f === null) {
-      return cards;
-    }
-    const filteredCards = cards.map(card => {
-      const res = card.filter(card => {
-        return card.priority === f;
-      });
-      return res;
-    });
+  // const filtered = (f, cards) => {
+  //   if (f === null) {
+  //     return cards;
+  //   }
+  //   const filteredCards = cards.map(card => {
+  //     const res = card.filter(card => {
+  //       return card.priority === f;
+  //     });
+  //     return res;
+  //   });
 
-    return filteredCards;
-  };
+  //   return filteredCards;
+  // };
 
-  const result = filtered(currentFilter, cards);
+  // const result = filtered(currentFilter, cards);
 
-  useEffect(() => {
-    console.log(result);
-  }, [result]);
+  // useEffect(() => {
+  //   console.log(result);
+  // }, [result]);
 
   return (
     <>

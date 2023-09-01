64c8fb8101398e36aa09355b468630f6fe3466ee
Maks <116167012+Str0ng3r@users.noreply.github.com>
2023-09-01 23:17:39 +0200

    Prettier

--- src/components/NewEditBoard/NewBoard.jsx
+++ src/components/NewEditBoard/NewBoard.jsx
@@ -3,11 +3,11 @@ import styles from './NewBoard.module.css';
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addBoard } from 'redux/boards/boardsOperations';
-const NewBoard = ({openModal}) => {
+const NewBoard = ({ openModal }) => {
   const [icons, setIcons] = useState('project');
   const [background, setBackground] = useState(null);
-const  [title,setTitle] = useState(null)
-const dispatch = useDispatch()
+  const [title, setTitle] = useState(null);
+  const dispatch = useDispatch();
   const getTitle = event => {
     setTitle(event.target.value);
   };
@@ -22,14 +22,14 @@ const dispatch = useDispatch()
   };
 
   const newBoardObject = {
-    title:title,
-    icon:icons,
-    background:background
-  }
+    title: title,
+    icon: icons,
+    background: background,
+  };
 
   const newBoardFunc = () => {
-dispatch(addBoard(newBoardObject))
-  }
+    dispatch(addBoard(newBoardObject));
+  };
 
   return (
     <div className={styles.divCard}>
@@ -48,94 +48,274 @@ dispatch(addBoard(newBoardObject))
             className={styles.buttonIcons}
             onClick={getIcon}
           >
-            <Icon id={'project'} className={icons === 'project' ? styles.serIcons : styles.darkIcons}></Icon>
+            <Icon
+              id={'project'}
+              className={
+                icons === 'project' ? styles.serIcons : styles.darkIcons
+              }
+            ></Icon>
           </button>
         </li>
         <li>
           <button onClick={getIcon} data-source="star">
-            <Icon id={'star'}className={icons === 'star' ? styles.serIcons : styles.darkIcons}></Icon>
+            <Icon
+              id={'star'}
+              className={icons === 'star' ? styles.serIcons : styles.darkIcons}
+            ></Icon>
           </button>
         </li>
         <li>
           <button onClick={getIcon} data-source="loading">
-            <Icon id={'loading'} className={icons === 'loading' ? styles.serIcons : styles.darkIcons}></Icon>
+            <Icon
+              id={'loading'}
+              className={
+                icons === 'loading' ? styles.serIcons : styles.darkIcons
+              }
+            ></Icon>
           </button>
         </li>
         <li>
           <button onClick={getIcon} data-source="puzzle-piece">
-            <Icon id={'puzzle-piece'} className={icons === 'puzzle-piece' ? styles.serIcons : styles.darkIcons}></Icon>
+            <Icon
+              id={'puzzle-piece'}
+              className={
+                icons === 'puzzle-piece' ? styles.serIcons : styles.darkIcons
+              }
+            ></Icon>
           </button>
         </li>
         <li>
           <button onClick={getIcon} data-source="container">
-            <Icon id={'container'} className={icons === 'container' ? styles.serIcons : styles.darkIcons}></Icon>
+            <Icon
+              id={'container'}
+              className={
+                icons === 'container' ? styles.serIcons : styles.darkIcons
+              }
+            ></Icon>
           </button>
         </li>
         <li>
           <button onClick={getIcon} data-source="lightning">
-            <Icon id={'lightning'} className={icons === 'lightning' ? styles.serIcons : styles.darkIcons}></Icon>
+            <Icon
+              id={'lightning'}
+              className={
+                icons === 'lightning' ? styles.serIcons : styles.darkIcons
+              }
+            ></Icon>
           </button>
         </li>
         <li>
           <button onClick={getIcon} data-source="colors">
-            <Icon id={'colors'} className={icons === 'colors' ? styles.serIcons : styles.darkIcons}></Icon>
+            <Icon
+              id={'colors'}
+              className={
+                icons === 'colors' ? styles.serIcons : styles.darkIcons
+              }
+            ></Icon>
           </button>
         </li>
         <li>
           <button onClick={getIcon} data-source="hexagon">
-            <Icon id={'hexagon'} className={icons === 'hexagon' ? styles.serIcons : styles.darkIcons}></Icon>
+            <Icon
+              id={'hexagon'}
+              className={
+                icons === 'hexagon' ? styles.serIcons : styles.darkIcons
+              }
+            ></Icon>
           </button>
         </li>
       </ul>
       <h3 className={styles.textBackground}>Background</h3>
       <ul className={styles.listColorIcons}>
-        <li className={background === "no-background" ? styles.listItemActive : styles.listItem }>
-          <button className={styles.buttonBack} onClick={getBack} data-source="no-background"></button>
+        <li
+          className={
+            background === 'no-background'
+              ? styles.listItemActive
+              : styles.listItem
+          }
+        >
+          <button
+            className={styles.buttonBack}
+            onClick={getBack}
+            data-source="no-background"
+          ></button>
         </li>
-        <li className={background === "magnolia" ? styles.listItemActive : styles.listItem}>
-          <button className={styles.buttonBack } onClick={getBack} data-source="magnolia"></button>
+        <li
+          className={
+            background === 'magnolia' ? styles.listItemActive : styles.listItem
+          }
+        >
+          <button
+            className={styles.buttonBack}
+            onClick={getBack}
+            data-source="magnolia"
+          ></button>
         </li>
-        <li className={background === "starry-sky" ? styles.listItemActive : styles.listItem }>
-          <button className={styles.buttonBack} onClick={getBack} data-source="starry-sky"></button>
+        <li
+          className={
+            background === 'starry-sky'
+              ? styles.listItemActive
+              : styles.listItem
+          }
+        >
+          <button
+            className={styles.buttonBack}
+            onClick={getBack}
+            data-source="starry-sky"
+          ></button>
         </li>
-        <li className={background === "sakura" ? styles.listItemActive : styles.listItem }>
-          <button className={styles.buttonBack} onClick={getBack} data-source="sakura"></button>
+        <li
+          className={
+            background === 'sakura' ? styles.listItemActive : styles.listItem
+          }
+        >
+          <button
+            className={styles.buttonBack}
+            onClick={getBack}
+            data-source="sakura"
+          ></button>
         </li>
-        <li className={background === "half-moon" ? styles.listItemActive : styles.listItem }>
-          <button className={styles.buttonBack} onClick={getBack} data-source="half-moon"></button>
+        <li
+          className={
+            background === 'half-moon' ? styles.listItemActive : styles.listItem
+          }
+        >
+          <button
+            className={styles.buttonBack}
+            onClick={getBack}
+            data-source="half-moon"
+          ></button>
         </li>
-        <li className={background === "palm-leaves" ? styles.listItemActive : styles.listItem }>
-          <button className={styles.buttonBack} onClick={getBack} data-source="palm-leaves"></button>
+        <li
+          className={
+            background === 'palm-leaves'
+              ? styles.listItemActive
+              : styles.listItem
+          }
+        >
+          <button
+            className={styles.buttonBack}
+            onClick={getBack}
+            data-source="palm-leaves"
+          ></button>
         </li>
-        <li className={background === "clouds" ? styles.listItemActive : styles.listItem }>
-          <button className={styles.buttonBack} onClick={getBack} data-source="clouds"></button>
+        <li
+          className={
+            background === 'clouds' ? styles.listItemActive : styles.listItem
+          }
+        >
+          <button
+            className={styles.buttonBack}
+            onClick={getBack}
+            data-source="clouds"
+          ></button>
         </li>
-        <li className={background === "rocky-beach" ? styles.listItemActive : styles.listItem }>
-          <button className={styles.buttonBack} onClick={getBack} data-source="rocky-beach"></button>
+        <li
+          className={
+            background === 'rocky-beach'
+              ? styles.listItemActive
+              : styles.listItem
+          }
+        >
+          <button
+            className={styles.buttonBack}
+            onClick={getBack}
+            data-source="rocky-beach"
+          ></button>
         </li>
-        <li className={background === "violet-circle" ? styles.listItemActive : styles.listItem }>
-          <button className={styles.buttonBack} onClick={getBack} data-source="violet-circle"></button>
+        <li
+          className={
+            background === 'violet-circle'
+              ? styles.listItemActive
+              : styles.listItem
+          }
+        >
+          <button
+            className={styles.buttonBack}
+            onClick={getBack}
+            data-source="violet-circle"
+          ></button>
         </li>
-        <li className={background === "full-moon" ? styles.listItemActive : styles.listItem }>
-          <button className={styles.buttonBack} onClick={getBack} data-source="full-moon"></button>
+        <li
+          className={
+            background === 'full-moon' ? styles.listItemActive : styles.listItem
+          }
+        >
+          <button
+            className={styles.buttonBack}
+            onClick={getBack}
+            data-source="full-moon"
+          ></button>
         </li>
-        <li className={background === "yacht" ? styles.listItemActive : styles.listItem }>
-          <button className={styles.buttonBack} onClick={getBack} data-source="yacht"></button>
+        <li
+          className={
+            background === 'yacht' ? styles.listItemActive : styles.listItem
+          }
+        >
+          <button
+            className={styles.buttonBack}
+            onClick={getBack}
+            data-source="yacht"
+          ></button>
         </li>
-        <li className={background === "baloon" ? styles.listItemActive : styles.listItem }>
-          <button className={styles.buttonBack} onClick={getBack} data-source="baloon"></button>
+        <li
+          className={
+            background === 'baloon' ? styles.listItemActive : styles.listItem
+          }
+        >
+          <button
+            className={styles.buttonBack}
+            onClick={getBack}
+            data-source="baloon"
+          ></button>
         </li>
-        <li className={background === "mountains" ? styles.listItemActive : styles.listItem }>
-          <button className={styles.buttonBack} onClick={getBack} data-source="mountains"></button>
+        <li
+          className={
+            background === 'mountains' ? styles.listItemActive : styles.listItem
+          }
+        >
+          <button
+            className={styles.buttonBack}
+            onClick={getBack}
+            data-source="mountains"
+          ></button>
         </li>
-        <li className={background === "sea" ? styles.listItemActive : styles.listItem }>
-          <button className={styles.buttonBack} onClick={getBack} data-source="sea"></button>
+        <li
+          className={
+            background === 'sea' ? styles.listItemActive : styles.listItem
+          }
+        >
+          <button
+            className={styles.buttonBack}
+            onClick={getBack}
+            data-source="sea"
+          ></button>
         </li>
-        <li className={background === "cappodocia" ? styles.listItemActive : styles.listItem }>
-          <button className={styles.buttonBack} onClick={getBack} data-source="cappodocia"></button>
+        <li
+          className={
+            background === 'cappodocia'
+              ? styles.listItemActive
+              : styles.listItem
+          }
+        >
+          <button
+            className={styles.buttonBack}
+            onClick={getBack}
+            data-source="cappodocia"
+          ></button>
         </li>
-        <li className={background === "night-trailer" ? styles.listItemActive : styles.listItem }>
-          <button className={styles.buttonBack} onClick={getBack} data-source="night-trailer"></button>
+        <li
+          className={
+            background === 'night-trailer'
+              ? styles.listItemActive
+              : styles.listItem
+          }
+        >
+          <button
+            className={styles.buttonBack}
+            onClick={getBack}
+            data-source="night-trailer"
+          ></button>
         </li>
       </ul>
       <button className={styles.mainButton} onClick={newBoardFunc}>

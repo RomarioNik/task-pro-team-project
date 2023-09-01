601554ade81a6fedc336b1ff705ec53f42b7e826
Zerossik <zeross19940303@gmail.com>
2023-09-01 21:14:13 +0300

    Update AddColumn.jsx

--- src/AddColumn/AddColumn.jsx
+++ src/AddColumn/AddColumn.jsx
@@ -1,14 +1,35 @@
 import { Button } from 'components/Button/Button';
 import css from './AddColumn.module.css';
 import { Icon } from 'components/Svg/Icon';
+import { useDispatch } from 'react-redux';
+import { addColumn } from 'redux/boards/boardsOperations';
+import { useParams } from 'react-router-dom';
 
 const AddColumn = () => {
+  const board = useParams(); //необхідно отримати id дошки!
+  const dispatch = useDispatch();
+  const handlerTitleColumn = evt => {
+    evt.preventDefault();
+    const inputValue = evt.target.title.value.trim();
+    if (inputValue !== '') {
+      console.log(board);
+      const newColumn = {
+        title: inputValue,
+        board: '', // необхідно передати id дошки!!
+      };
+      dispatch(addColumn(newColumn));
+      return;
+    }
+
+    return;
+  };
   return (
     <div className={css.container}>
       <p className={css.addColumn_title}>Add column</p>
-      <form>
+      <form onSubmit={handlerTitleColumn}>
         <input
           type="text"
+          name="title"
           placeholder="Title"
           className={css.addColumn_input}
           autoFocus

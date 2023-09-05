3d178c61fe87ac5efb5cc3e6ebb73ea47b167ef1
Zerossik <zeross19940303@gmail.com>
2023-09-05 18:01:57 +0300

    changeColumn

--- src/components/ChangeColumn/ChangeColumn.jsx
+++ src/components/ChangeColumn/ChangeColumn.jsx
@@ -2,14 +2,14 @@ import { useShownBoard } from 'hooks/useShownBoard';
 import css from './ChangeColumn.module.css';
 import { Button } from 'components/Button/Button';
 const ChangeColumn = ({ id }) => {
-  const columns = useShownBoard().columns.map(({ title }) => title);
+  const columns = useShownBoard().columns;
 
   return (
     <div className={css.container}>
       <ul className={css.list}>
         {columns.map((el, index) => (
           <li key={index}>
-            <Button className={css.item}>{el}</Button>
+            <Button className={css.item}>{el.title}</Button>
           </li>
         ))}
       </ul>

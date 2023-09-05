6a07eb7a0fdb322eb155459def199f8ab6370d84
Zerossik <zeross19940303@gmail.com>
2023-09-05 19:12:03 +0300

    Update ChangeColumn.jsx

--- src/components/ChangeColumn/ChangeColumn.jsx
+++ src/components/ChangeColumn/ChangeColumn.jsx
@@ -1,24 +1,25 @@
 import { useShownBoard } from 'hooks/useShownBoard';
 import css from './ChangeColumn.module.css';
 import { Button } from 'components/Button/Button';
-// import { useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { transportCard } from 'redux/boards/boardsOperations';
 const ChangeColumn = ({ columnId, handlerClose, cardId }) => {
   const columns = useShownBoard().columns.filter(({ _id }) => _id !== columnId);
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
   const handlerClick = newColumnId => {
     console.log(columnId, '-ид старой колонки');
     console.log(cardId, '-ид карточки');
     console.log(newColumnId, '- новый ид');
 
-    // const changeColumnData = {
-    //   _id: cardId,
-    //   moveData: {
-    //     source: columnId,
-    //     destination: newColumnId,
-    //   },
-    // };
-    // dispatch()
+    const changeColumnData = {
+      _id: cardId,
+      moveData: {
+        source: columnId,
+        destination: newColumnId,
+      },
+    };
+    dispatch(transportCard(changeColumnData));
     handlerClose();
   };
   return (

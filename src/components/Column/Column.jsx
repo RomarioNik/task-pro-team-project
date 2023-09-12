156279fddeebe27724b0e7e4064faae2f3a3ebb6
Illia Datsenko <datsenko.i@gmail.com>
2023-09-12 21:30:34 +0300

    dnd lib

--- src/components/Column/Column.jsx
+++ src/components/Column/Column.jsx
@@ -16,6 +16,8 @@ import { useShownBoard } from 'hooks/useShownBoard';
 import TaskCard from 'components/TaskCard/TaskCard';
 import { getFilter } from 'redux/boards/boardsSelectors';
 
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+
 const Column = () => {
   const [isOpenModalAddColumn, setIsOpenModalAddColumn] = useState(false);
   const [isOpenModalEditColumn, setIsOpenModalEditColumn] = useState(false);
@@ -27,6 +29,50 @@ const Column = () => {
   const dispatch = useDispatch();
 
   const shownBoard = useShownBoard();
+  console.log(shownBoard);
+
+  const reorder = (list, startIndex, endIndex) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+
+    return result;
+  };
+
+  const move = (source, destination, droppableSource, droppableDestination) => {
+    const sourceClone = Array.from(source);
+    const destClone = Array.from(destination);
+    const [removed] = sourceClone.splice(droppableSource.index, 1);
+
+    destClone.splice(droppableDestination.index, 0, removed);
+
+    const result = {};
+    result[droppableSource.droppableId] = sourceClone;
+    result[droppableDestination.droppableId] = destClone;
+
+    return result;
+  };
+
+  const grid = 8;
+
+  const getItemStyle = (isDragging, draggableStyle) => ({
+    // some basic styles to make the items look a bit nicer
+    userSelect: 'none',
+    padding: grid * 2,
+    margin: `0 0 ${grid}px 0`,
+
+    // change background colour if dragging
+    background: isDragging ? 'lightgreen' : 'grey',
+
+    // styles we need to apply on draggables
+    ...draggableStyle,
+  });
+
+  const getListStyle = isDraggingOver => ({
+    background: isDraggingOver ? 'lightblue' : 'lightgrey',
+    padding: grid,
+    width: 250,
+  });
 
   const allColumns = shownBoard.columns;
 
@@ -46,6 +92,33 @@ const Column = () => {
   });
 
   const columns = currentFilter === '' ? allColumns : filteredColumns;
+
+  const [state, setState] = useState(columns);
+
+  function onDragEnd(result) {
+    const { source, destination } = result;
+
+    // dropped outside the list
+    if (!destination) {
+      return;
+    }
+    const sInd = +source.droppableId;
+    const dInd = +destination.droppableId;
+
+    if (sInd === dInd) {
+      const items = reorder(state[sInd], source.index, destination.index);
+      const newState = [...state];
+      newState[sInd] = items;
+      setState(newState);
+    } else {
+      const result = move(state[sInd], state[dInd], source, destination);
+      const newState = [...state];
+      newState[sInd] = result[sInd];
+      newState[dInd] = result[dInd];
+
+      setState(newState.filter(group => group.length));
+    }
+  }
 
   useEffect(() => {
     const handleResize = () => {
@@ -68,44 +141,33 @@ const Column = () => {
     setIsOpenModalAddCard(!isOpenModalAddCard);
   };
 
-  //----------------скрол-віріант-1-(робочий)---------------------------
-  // const columnWrapper = useRef(null);
-  // const hasScroll = columns.length >= 1;
-  // const hasScroll = true;
-
-  // useScrollbar(columnWrapper, hasScroll);
-
   return (
-    <div
-    // style={{ width: hasScroll ? '100%' : 'auto', minWidth: '320px' }}
-    // ref={columnWrapper}
-    >
+    <div>
       <ul className={style.column__item}>
-        {columns.length !== 0 &&
-          columns.map(({ title, _id, cards }) => (
-            <li key={_id} className={style.column}>
-              <div className={style.column__section}>
-                <p className={style.column_title}>{title}</p>
+        <DragDropContext onDragEnd={onDragEnd}>
+          {columns.length !== 0 &&
+            columns.map(({ title, _id, cards }, ind) => (
+              <li key={_id} className={style.column}>
+                <div className={style.column__section}>
+                  <p className={style.column_title}>{title}</p>
 
-                <div className={style.column__edit__button}>
-                  <button
-                    onClick={e => {
-                      handlerEditColum();
-                      setIdColumn(_id);
-                    }}
-                  >
-                    <Icon id="pencil" className={style.column__icon} />
-                  </button>
-                  <button onClick={() => dispatch(deleteColumn(_id))}>
-                    <Icon id="trash" className={style.column__icon} />
-                  </button>
+                  <div className={style.column__edit__button}>
+                    <button
+                      onClick={e => {
+                        handlerEditColum();
+                        setIdColumn(_id);
+                      }}
+                    >
+                      <Icon id="pencil" className={style.column__icon} />
+                    </button>
+                    <button onClick={() => dispatch(deleteColumn(_id))}>
+                      <Icon id="trash" className={style.column__icon} />
+                    </button>
+                  </div>
                 </div>
-              </div>
-              {/* <OverlayScrollbarsComponent> */}
-                <div 
+                <div
                   className={style.card__container}
                   style={{ maxHeight: `${containerHeight - 300}px` }}
-                  // style={{ height: `1920px` }}
                 >
                   <ul className={style.scroll__container}>
                     {cards &&
@@ -116,24 +178,25 @@ const Column = () => {
                       ))}
                   </ul>
                 </div>
-              {/* </OverlayScrollbarsComponent> */}
 
-              <button
-                className={style.button_create}
-                type="button"
-                // onClick={addCard}
-                onClick={() => {
-                  addCard();
-                  setIdColumn(_id);
-                }}
-              >
-                <div className={style.button__icon__background}>
-                  <Icon id="plus" className={style.button__create__icon} />
-                </div>
-                <p className={style.button_create_title}>Add another card</p>
-              </button>
-            </li>
-          ))}
+                <button
+                  className={style.button_create}
+                  type="button"
+                  // onClick={addCard}
+                  onClick={() => {
+                    addCard();
+                    setIdColumn(_id);
+                  }}
+                >
+                  <div className={style.button__icon__background}>
+                    <Icon id="plus" className={style.button__create__icon} />
+                  </div>
+                  <p className={style.button_create_title}>Add another card</p>
+                </button>
+              </li>
+            ))}
+        </DragDropContext>
+
         <li className={style.column}>
           <button
             className={style.button__create__column}
@@ -177,147 +240,3 @@ const Column = () => {
 };
 
 export default Column;
-
-// import React, { useRef, useState } from 'react';
-// import { Icon } from '../Svg/Icon';
-// import useScrollbar from '../Scroll/index';
-// import Modal from 'components/Modal/Modal';
-// import AddColumn from 'components/AddColumn';
-// import EditColumn from 'components/EditColumn';
-// import Card from 'components/Card/Card';
-// import CreateCardPopUp from 'components/CreateCardPopUp/CreateCardPopUp';
-// import { deleteColumn } from 'redux/boards/boardsOperations';
-// import 'overlayscrollbars/overlayscrollbars.css';
-// import style from './Column.module.css';
-// import { useDispatch } from 'react-redux';
-
-// import { useBoardsList } from 'hooks/useBoardsList';
-
-// const columns = [
-//   {
-//     title: 'To Do',
-//     _id: '1',
-//   },
-//   {
-//     title: 'In progress',
-//     _id: '2',
-//   },
-//   {
-//     title: 'Done',
-//     _id: '3',
-//   },
-//   {
-//     title: 'Star',
-//     _id: '4',
-//   },
-// ];
-
-// const Column = () => {
-//   const [isOpenModalAddColumn, setIsOpenModalAddColumn] = useState(false);
-//   const [isOpenModalEditColumn, setIsOpenModalEditColumn] = useState(false);
-//   const [getIdColumn, setIdColumn] = useState(null);
-//   const [isOpenModalAddCard, setIsOpenModalAddCard] = useState(false);
-
-//   const dispatch = useDispatch();
-
-//   const boards = useBoardsList();
-//   console.log(boards)
-
-//   // const {columns} = boards
-//   // console.log(columns)
-
-//   const addColumn = () => {
-//     setIsOpenModalAddColumn(!isOpenModalAddColumn);
-//   };
-
-//   const handlerEditColum = () => {
-//     setIsOpenModalEditColumn(!isOpenModalEditColumn);
-//   };
-
-//   const addCard = () => {
-//     setIsOpenModalAddCard(!isOpenModalAddCard);
-//   };
-
-//   // const deleteColumn =() => {
-//   //   return alert('STOP!')
-//   // }
-
-//   //----------------скрол-віріант-1-(робочий)---------------------------
-//   const columnWrapper = useRef(null);
-//   const hasScroll = columns.length >= 1;
-
-//   useScrollbar(columnWrapper, hasScroll);
-
-//   return (
-//     <div
-//       style={{ width: hasScroll ? '100%' : 'auto', minWidth: '320px' }}
-//       ref={columnWrapper}
-//     >
-//       <ul className={style.column__item}>
-//         {columns.map(({ title, _id, cards }) => (
-//           <li key={_id} className={style.column}>
-//             <div className={style.column__section}>
-//               <p className={style.column_title}>{title}</p>
-
-//               <div className={style.column__edit__button}>
-//                 <button
-//                   onClick={e => {
-//                     handlerEditColum();
-//                     setIdColumn(_id);
-//                   }}
-//                 >
-//                   <Icon id="pencil" className={style.column__icon} />
-//                 </button>
-//                 <button onClick={() => dispatch(deleteColumn(_id))}>
-//                   <Icon id="trash" className={style.column__icon} />
-//                 </button>
-//               </div>
-//             </div>
-//             <div className={style.card__container}>
-//               <Card />
-//             </div>
-//             <button
-//               className={style.button_create}
-//               type="button"
-//               onClick={addCard}
-//             >
-//               <div className={style.button__icon__background}>
-//                 <Icon id="plus" className={style.button__create__icon} />
-//               </div>
-//               <p className={style.button_create_title}>Add another card</p>
-//             </button>
-//           </li>
-//         ))}
-//         <li>
-//           <button
-//             className={style.button__create__column}
-//             type="button"
-//             onClick={addColumn}
-//           >
-//             <div className={style.button__icon__bg}>
-//               <Icon id="plus" className={style.button__icon} />
-//             </div>
-//             <p className={style.button__title}>Add another column</p>
-//           </button>
-//         </li>
-//       </ul>
-//       {isOpenModalAddColumn && (
-//         <Modal openModal={addColumn}>
-//           <AddColumn closeModal={addColumn} />
-//         </Modal>
-//       )}
-//       {isOpenModalEditColumn && (
-//         <Modal openModal={handlerEditColum}>
-//           <EditColumn id={getIdColumn} close={handlerEditColum} />
-//         </Modal>
-//       )}
-//       {isOpenModalAddCard && (
-//         <Modal openModal={addCard}>
-//           <CreateCardPopUp />
-//         </Modal>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default Column;
